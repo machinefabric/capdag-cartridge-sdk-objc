49cffb9d822df0b0828aee2a8263f6ace5a847d7
Bahram Shamshiri <jowharshamshiri@gmail.com>
2026-08-15 00:05:27 +0200

    Update capdag-objc dependency to version 1.416.18

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
             targets: ["MachFabCartridgeSDKSwift"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/machinefabric/capdag-objc.git", from: "1.411.3"),
+        .package(url: "https://github.com/machinefabric/capdag-objc.git", from: "1.416.18"),
     ],
     targets: [
         .target(
