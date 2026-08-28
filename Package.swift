eb5bb46d687aa51907bd66b312e8ff2078f1f689
Bahram Shamshiri <jowharshamshiri@gmail.com>
2026-08-28 18:54:16 +0200

    v bump

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// version: 1.132.0
+// version: 1.133.2
 // swift-tools-version: 6.0
 
 import PackageDescription
@@ -24,7 +24,7 @@ let package = Package(
             targets: ["CapDAGCartridgeSDKSwift"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/machinefabric/capdag-objc.git", from: "1.447.53"),
+        .package(url: "https://github.com/machinefabric/capdag-objc.git", from: "1.448.0"),
     ],
     targets: [
         .target(
